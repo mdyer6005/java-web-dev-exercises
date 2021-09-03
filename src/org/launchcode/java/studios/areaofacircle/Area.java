ea58752f77612f04da2b3d5c5db8e8463dc73ebd
Matt <dyermcd@gmail.com>
2021-09-02 21:22:23 -0500

    updated Studio 01 with some of Anastasia's input validation

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -1,13 +1,25 @@
 package org.launchcode.java.studios.areaofacircle;
+
 import java.util.Scanner;
+
+import static org.launchcode.java.studios.areaofacircle.CheckInput.isValidDouble;
 import static org.launchcode.java.studios.areaofacircle.Circle.getArea;
 
 public class Area {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a radius: ");
-        Double radius = input.nextDouble();
-        Double area = getArea(radius);
-        System.out.println("The area of a circle with a radius of " + radius + " is: " + area);
+        String userInput = input.nextLine();
+        while (!isValidDouble(userInput)) {
+            System.out.println("Invalid radius. Enter a number: ");
+            userInput = input.nextLine();
+        }
+        try {
+            Double radius = Double.parseDouble(userInput);
+            Double area = getArea(radius);
+            System.out.println("The area of a circle with a radius of " + radius + " is: " + area);
+        } catch (NumberFormatException e) {
+            System.out.println("Input String cannot be parsed to Double.");
+        }
     }
 }
---
+++ src/org/launchcode/java/studios/areaofacircle/CheckInput.java
@@ -0,0 +1,18 @@
+package org.launchcode.java.studios.areaofacircle;
+
+public class CheckInput {
+    public static boolean isValidDouble(String inputString) {
+        double doubleValue;
+
+        if (inputString == null || inputString.equals("")) {
+            return false;
+        }
+        try {
+            doubleValue = Double.parseDouble(inputString);
+            return true;
+        } catch (NumberFormatException e) {
+            System.out.println("Input String cannot be parsed to Double.");
+        }
+        return false;
+    }
+}
