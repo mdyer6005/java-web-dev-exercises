a5b1f3f43e5b564bd4d1fb34c5dc566653e46874
Matt <dyermcd@gmail.com>
2021-09-07 19:53:33 -0500

    merged studio 02 into main

---
+++ src/studios/class01/Area.java
@@ -0,0 +1,25 @@
+package studios.class01;
+
+import java.util.Scanner;
+
+import static studios.class01.CheckInput.isValidDouble;
+import static studios.class01.Circle.getArea;
+
+public class Area {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a radius: ");
+        String userInput = input.nextLine();
+        while (!isValidDouble(userInput)) {
+            System.out.println("Invalid radius. Enter a number: ");
+            userInput = input.nextLine();
+        }
+        try {
+            Double radius = Double.parseDouble(userInput);
+            Double area = getArea(radius);
+            System.out.println("The area of a circle with a radius of " + radius + " is: " + area);
+        } catch (NumberFormatException e) {
+            System.out.println("Input String cannot be parsed to Double.");
+        }
+    }
+}
---
+++ src/studios/class01/Circle.java
@@ -0,0 +1,7 @@
+package studios.class01;
+
+public class Circle {
+    public static Double getArea(Double radius) {
+        return (radius * radius) * Math.PI;
+    }
+}
