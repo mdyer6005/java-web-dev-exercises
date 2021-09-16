615a2353ce685410a8e7b13039888ca6e7c15516
Matt <dyermcd@gmail.com>
2021-09-15 21:20:10 -0500

    completed class 05 exercises

--- src/exercises/class05/chap06/test/CarTest.java
+++ src/exercises/class05/chap06/test/CarTest.java
@@ -1,8 +1,10 @@
-package org.launchcode.java.demos.lsn5unittesting.test;
+package exercises.class05.chap06.test;
+import exercises.class05.chap06.main.Car;
 import org.junit.Before;
 import org.junit.Test;
-import org.launchcode.java.demos.lsn5unittesting.main.Car;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class CarTest {
     Car test_car;
@@ -22,9 +24,26 @@ public class CarTest {
     public void testInitialGasTank() {
         assertEquals(10, test_car.getGasTankLevel(), .001);
     }
+
     //TODO: gasTankLevel is accurate after driving within tank range
+    @Test
+    public void testGasTankAfterDriving() {
+        test_car.drive(50);
+        assertEquals(9, test_car.getGasTankLevel(), .001);
+    }
 
     //TODO: gasTankLevel is accurate after attempting to drive past tank range
+    @Test
+    public void testGasTankAfterExceedingTankRange() {
+        test_car.drive(7000);
+        assertEquals(0, test_car.getGasTankLevel(), .001);
+    }
+
     //TODO: can't have more gas than tank size, expect an exception
+    @Test(expected = IllegalArgumentException.class)
+    public void testGasOverfillException() {
+        test_car.addGas(5);
+        fail("Shouldn't get here, car cannot have more gas in the tank than the size of the tank");
+    }
 
 }
